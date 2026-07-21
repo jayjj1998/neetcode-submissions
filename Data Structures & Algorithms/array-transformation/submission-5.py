bf293b29cca0ec5ca1137c280252d9c6decd122d
jayjj1998 <78464395+jayjj1998@users.noreply.github.com>
2026-07-21 10:03:37 -0500

    Add: array-transformation - submission-5

---
+++ Data Structures & Algorithms/array-transformation/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def transformArray(self, arr: List[int]) -> List[int]:
+
+        current_day = arr.copy()
+        oper_exec = False
+
+        while True:
+            for i in range(1, len(arr) - 1):
+                if current_day[i - 1] > current_day[i] < current_day[i + 1]:
+                    arr[i] += 1
+                    oper_exec = True
+                elif current_day[i - 1] < current_day[i] > current_day[i + 1]:
+                    arr[i] -= 1
+                    oper_exec = True
+                
+            if not oper_exec:
+                break
+
+            current_day = arr.copy()
+            oper_exec = False
+        
+        return arr
